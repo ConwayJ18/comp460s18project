3c531c66a0c8270d5393e12139d1978344d3fa0c
ConwayJ18 <conway.jess18@gmail.com>
2018-04-03 19:52:14 -0500

    Updated Driver for PollardRho and Dixon limitations.

--- Driver.java
+++ Driver.java
@@ -4,12 +4,15 @@ import java.math.*;
 import src.millerrabin.*;
 import src.semiprime.*;
 import src.dixon.*;
+import src.pollardrho.*;
 
 class Driver {
 
     public static void main(String[] args)
     {
         boolean running = true;
+        BigInteger MAX_Dixon = new BigInteger("180250");
+        //BigInteger MAX_PollardRho = new BigInteger("0");
 
         while(running)
         {
@@ -26,8 +29,16 @@ class Driver {
                 System.out.println(testNumber + " is composite? " + isComposite); //Test for primality
                 if(isComposite)
                 {
-                  System.out.println(testNumber + " is semi-prime? " + SemiPrime.isSemiPrime(testNumber)); //Test if semiPrime
-                  System.out.println("The factors of " + testNumber + " are " + Dixon.dixon(testNumber)); //List factors
+                  System.out.println(testNumber + " is semi-prime? " + SemiPrime.isSemiPrime(testNumber)); //Run SemiPrime
+                  System.out.println("The factors of " + testNumber + " via Pollard-Rho are " + PollardRho.pollardRho(testNumber)); //Run PollardRho
+                  if(testNumber.compareTo(MAX_Dixon) <= 0)
+                  {
+                    System.out.println("The factors of " + testNumber + " via Dixon are " + Dixon.dixon(testNumber)); //Run Dixon
+                  }
+                  else
+                  {
+                    System.out.println("The test number is too large for our Dixon algorithm.");
+                  }
                 }
                 else
                 {
