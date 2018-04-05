71d32fb1e6000e5434bf05307489e099451012c9
ConwayJ18 <conway.jess18@gmail.com>
2018-04-05 14:13:27 -0500

    Fixed PollardRho

--- src/pollardrho/PollardRho.java
+++ src/pollardrho/PollardRho.java
@@ -9,28 +9,30 @@ public class PollardRho {
       BigInteger a = new BigInteger("2");
       BigInteger b = new BigInteger("2");
       BigInteger d = BigInteger.ONE;
-      while (d.compareTo(BigInteger.ONE) == 0) {
+      while (d.compareTo(BigInteger.ONE) == 0) { //While d is equal to 1
           //a <- a^2+1 mod n
-          a = a.pow(2);
-          a = a.add(BigInteger.ONE);
-          a = a.mod(n);
+          a = a.pow(2); //Square a
+          a = a.add(BigInteger.ONE); //Add 1
+          a = a.mod(n); //Mod n
 
           //b <- b^2+1 mod n
-          b = b.pow(2);
-          b = b.add(BigInteger.ONE);
-          b = b.mod(n);
+          for(int i=0; i<2; i++){
+              b = b.pow(2); //Square b
+              b = b.add(BigInteger.ONE); //Add 1
+              b = b.mod(n); //Mod n
+          }
 
           //d = gcd(a-b, n)
           d = a;
           d = d.subtract(b);
           d = d.gcd(n);
 
-          if (d.compareTo(n) == 0) {
+          if (d.compareTo(n) == 0) { //If d is n, number is prime (See Fermat's Little Theorem)
               return "Number is prime.";
           }
       }
-      BigInteger factor = n.divide(d);
-      return "" + d + " x " + factor;
+      BigInteger factor = n.divide(d); //Divide out d from the original number
+      return "" + d + " x " + factor; //Print d and the other factor
   }
 
 	public static void main(String[] args) //To test this class. Actual main() is in Driver.java
